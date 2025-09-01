a0676587c613b8e99bf3c3ab2520f769b1caad03
KartikayChaudhary-hax <namankartikay94@gmail.com>
2025-09-01 11:47:06 +0530

    100 Days of Code-Day 6
    Q-11:User inputs an integer and whether the number is odd or even,it is displayed on the screen.

---
+++ oddeven.c
@@ -0,0 +1,30 @@
+/*
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Enter an integer:");
+    int i;
+    scanf("%d", &i);
+    if(i%2==0)
+    {
+        printf("%d is even", i);
+    }
+    else
+    {
+        printf("%d is odd", i);
+    }
+    return 0;
+}
