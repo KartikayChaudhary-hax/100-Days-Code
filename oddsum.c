801caeae1d4157810e0eed40d5b2c9deb6ac2de2
KartikayChaudhary-hax <namankartikay94@gmail.com>
2025-09-16 14:44:01 +0530

    100 Days of Code-Day 14
    Q-27: User inputs a number n the sum of first n odd numbers is printed.

---
+++ oddsum.c
@@ -0,0 +1,23 @@
+/*
+Q27: Write a program to print the sum of the first n odd numbers.
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Enter a number:");
+    int d;
+    scanf("%d",&d);
+    int sum=d*d;
+    printf("The sum of first %d odd numbers is:%d",d,sum);
+    return 0;
+}
