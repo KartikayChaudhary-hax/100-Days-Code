0924299a31b92e49305caa4bed57f6af83855c00
KartikayChaudhary-hax <namankartikay94@gmail.com>
2025-09-17 22:37:05 +0530

    100 Days of Code-Day 15
    Q-29:User inputs a number n and its factorial is displayed.

---
+++ factorial.c
@@ -0,0 +1,28 @@
+/*
+Q29: Write a program to calculate the factorial of a number.
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Enter a number:");
+    int n;
+    scanf("%d",&n);
+    int fac=1;
+    for(int i=n;i>=1;i--)
+    {
+      fac=fac*i;
+    }
+    printf("The factorial of %d is:%d",n,fac);
+    return 0;
+}
